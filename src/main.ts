808dba883110d23f5e158b542f2e5ac8a71192c3
devtipso <80788568+devtipso@users.noreply.github.com>
2021-03-16 21:48:13 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,10 @@ async function bootstrap() {
   const allowedOrigins = [
     'http://localhost:4200',
     'https://auclair.000webhostapp.com',
-    'http://app-47c6a1e2-2f97-4638-a62d-7157933d0a8d.cleverapps.io/'
+    'http://app-47c6a1e2-2f97-4638-a62d-7157933d0a8d.cleverapps.io',
+    'http://app-47c6a1e2-2f97-4638-a62d-7157933d0a8d.cleverapps.io/',
+    'http://app-8c28d2f9-3afa-4776-a71f-1a78d26bd1c2.cleverapps.io',
+    'http://app-8c28d2f9-3afa-4776-a71f-1a78d26bd1c2.cleverapps.io/'
   ];
 
   const corsOptions = {
