c826326bb3aab5ea6f4707005c71350a4afd1763
devtipso <80788568+devtipso@users.noreply.github.com>
2021-03-30 17:54:34 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     },
   };
   app.enableCors({
-            origin: true,
+            origin: false,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
             credentials: true,
         });
